06d10335a15bd44d741e3de98ce416cbda8d34ba
Alejandro Collado Severiano <colladoacs@gmail.com>
2026-04-29 23:09:30 +0200

    Add files via upload

---
+++ src/main/java/com/tiendaonline/gestion/dto/auth/pedido/ItemPedidoRequest.java
@@ -0,0 +1,28 @@
+package com.tiendaonline.gestion.dto.pedido;
+
+public class ItemPedidoRequest {
+	
+	
+	private Long productoId;
+	private Integer cantidad;
+	
+	public ItemPedidoRequest() {
+		super();
+	}
+
+	public Long getProductoId() {
+		return productoId;
+	}
+	
+	public void setProductoId(Long productoId) {
+		this.productoId = productoId;
+	}
+	
+	public Integer getCantidad() {
+		return cantidad;
+	}
+	
+	public void setCantidad(Integer cantidad) {
+		this.cantidad = cantidad;
+	}
+}
