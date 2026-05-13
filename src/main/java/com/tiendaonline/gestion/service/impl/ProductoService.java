31d25268f090efef6b99f379a88b9a9414b89028
Alejandro Collado Severiano <colladoacs@gmail.com>
2026-05-13 14:14:47 +0200

    Add files via upload

---
+++ src/main/java/com/tiendaonline/gestion/service/impl/ProductoService.java
@@ -0,0 +1,27 @@
+package com.tiendaonline.gestion.service;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+
+import com.tiendaonline.gestion.dto.producto.ProductoResponse;
+import com.tiendaonline.gestion.model.Producto;
+
+public interface ProductoService {
+	
+	Producto crearProducto(Producto producto);
+	
+	Producto actualizarProducto(Long id, Producto producto);
+	
+	void eliminarProducto(Long id);
+	
+	ProductoResponse obtenerPorId(Long id);
+	
+	List<ProductoResponse> listarTodos();
+	
+	Page<ProductoResponse> listarProductosPaginados(int page, int size);
+	
+	Page<ProductoResponse> filtrarPrductos(Long categoriaId, BigDecimal precioMin, BigDecimal precioMax, int page, int size);
+
+}
