f5f04f0cb340f1cd28653ee023f5362da9f13d8a
Alejandro Collado Severiano <colladoacs@gmail.com>
2026-05-20 15:27:31 +0200

    Add files via upload

--- src/main/java/com/tiendaonline/gestion/dto/pedido/ItemPedidoRequest.java
+++ src/main/java/com/tiendaonline/gestion/dto/pedido/ItemPedidoRequest.java
@@ -1,10 +1,18 @@
 package com.tiendaonline.gestion.dto.pedido;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
 public class ItemPedidoRequest {
 	
-	
+	@NotNull(message = "El producto es obligatorio")
 	private Long productoId;
+	
+	@NotNull(message = "La cantidad es obligatoria")
+	@Min(value = 1, message = "La cantidad debe ser mayor de 0")
 	private Integer cantidad;
+	
+	
 	
 	public ItemPedidoRequest() {
 		super();
