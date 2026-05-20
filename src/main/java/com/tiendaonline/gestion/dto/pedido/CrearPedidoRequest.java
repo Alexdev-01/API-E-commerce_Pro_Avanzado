1baa1b8405f6cff6c5631922d909b390464bab12
Alejandro Collado Severiano <colladoacs@gmail.com>
2026-05-20 15:30:52 +0200

    Add files via upload

--- src/main/java/com/tiendaonline/gestion/dto/pedido/CrearPedidoRequest.java
+++ src/main/java/com/tiendaonline/gestion/dto/pedido/CrearPedidoRequest.java
@@ -2,9 +2,15 @@ package com.tiendaonline.gestion.dto.pedido;
 
 import java.util.List;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
+
 public class CrearPedidoRequest {
 	
-	private List<ItemPedidoRequest> items;
+	@NotEmpty(message = "La lista debe tener al menos un producto")
+	@Valid	// Valida cada elemento de la lista
+	private List<ItemPedidoRequest> items;	
+	
 	
 	public List<ItemPedidoRequest> getItems() {
 		return items;
